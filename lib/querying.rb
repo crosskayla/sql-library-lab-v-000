c481ccce0921a96ad40e7d31e285ceaa409d3e2c
Kayla Cross <kac288@georgetown.edu>
2018-05-28 22:28:24 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -58,7 +58,8 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name
   FROM characters
-  INNER JOIN
+  INNER JOIN character_books
+  ON characters.
 
   "
 end
